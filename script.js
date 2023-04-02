d3229574533541ddf268773ed12855f595ae9e23
Alejandro Diaz <Alejodv562@gmail.com>
2023-04-02 18:28:28 -0500

    metodos faltantes terminados

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const d = document,
     $table = d.querySelector(".viewUsers_table"),
-    $tbody = $table.querySelector("tbody")
+    $tbody = $table.querySelector("tbody"),
+    $form = d.querySelector(".editUsers_form"),
+    $editUser = d.querySelector(".editUsers_title")
 
 const getUsers = async()=>{
     try {
-        const url = "http://localhost:3000/users",
+        const url = "http://localhost:5000/users",
         res = await fetch(url)
         if(res.status === 200){
             data =  await res.json()
@@ -13,11 +15,11 @@ const getUsers = async()=>{
             data.forEach(el => {
                 contenido += `
                 <tr>
-                <td class="name">${el.name}</td>
-                <td class="lastName">${el.lastName}</td>
+                <td class="name" >${el.name}</td>
+                <td class="lastName" >${el.lastName}</td>
                 <td class="actions">
-                    <button class="edit">editar</button>
-                    <button class="delete">eliminar</button>
+                    <button class="edit" data-name="${el.name}" data-lastname="${el.lastName}" data-id="${el.id}">editar</button>
+                    <button class="delete" data-id="${el.id}">eliminar</button>
                 </td>
                 </tr>
                 `  
@@ -35,3 +37,78 @@ const getUsers = async()=>{
 }
 
 d.addEventListener("DOMContentLoaded", getUsers())
+
+d.addEventListener("click", async e =>{
+    if(e.target.matches(".edit")){
+        $editUser.textContent = "Editar Usuario"
+        $form.name.value = e.target.dataset.name
+        $form.lastName.value = e.target.dataset.lastname
+        $form.id.value = e.target.dataset.id
+    }
+
+    if(e.target.matches(".delete")){
+        try {
+            const url = `http://localhost:5000/users/${e.target.dataset.id}`,
+                options= {
+                    method: "DELETE",
+                },
+                res = await fetch(url, options)
+
+        } catch (error) {
+            
+        }
+    }
+})
+
+d.addEventListener("submit", async e=>{
+    if(e.target === $form){
+        e.preventDefault()
+
+        if(!e.target.id.value){
+            //CREATE - POST
+
+            try {
+                const url = `http://localhost:5000/users`,
+                    options = {
+                        method: "POST",
+                        headers: {
+                            "Content-Type":"application/json"
+                        },
+                        body: JSON.stringify({
+                            "name": e.target.name.value,
+                            "lastName": e.target.lastName.value
+
+                        })
+                    },
+                    res = await fetch(url, options)
+
+                    if(!res.ok) throw {status: res.status, statusText: res.statusText}
+            } catch (error) {
+                console.log(error)
+                let message = error.statusText || "ocurrio un error"
+                $form.insertAdjacentHTML("afterend", `<p><b>${error.status}: ${message}</b></p>`)  
+            }
+
+        }else{
+            try {
+                const url = `http://localhost:5000/users/${e.target.id.value}`,
+                    options = {
+                        method: "PUT",
+                        headers: {
+                            "Content-Type":"application/json"
+                        },
+                        body: JSON.stringify({
+                            "name": e.target.name.value,
+                            "lastName": e.target.lastName.value
+                        })
+                    },
+                    res = await fetch(url, options)
+
+                    if(!res.ok) throw {status:res.status, statusText:res.statusText}
+            } catch (error) {
+                $form.insertAdjacentHTML("afterend", `<p><b>${error.status}: ${error.statusText}</b></p>`)
+            }
+        }
+    }
+
+})
